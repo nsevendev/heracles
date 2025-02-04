ab98b05cfe27863e3290e4ddbcd66323fe16faf6
BoissonMathis <math.boiss@laposte.net>
2025-02-04 14:13:16 +0100

    ca tourne avec un token

--- src/core/pages/public/todolist-page/service/methods.service.ts
+++ src/core/pages/public/todolist-page/service/methods.service.ts
@@ -2,33 +2,36 @@ import { Inject, Injectable } from '@angular/core';
 import { patchState } from '@ngrx/signals';
 import { Task } from '../model/todolist.model';
 import { STORE_TOKEN } from '../store/store.token';
+import { TodolistState } from '../store/todolist.store';
 
 @Injectable()
 export class MethodsService {
   constructor(@Inject(STORE_TOKEN) private store: any) {}
 
   addTask = (content: string) => {
-    patchState(this.store, () => {
+    console.log(this.store);
+    patchState(this.store, (state: TodolistState) => {
+      console.log(state);
       const newTask: Task = {
         id: Date.now(),
         content,
         completed: false,
       };
-      return { tasks: [...this.store.tasks, newTask] };
+      return { tasks: [...state.tasks, newTask] };
     });
   };
 
   toggleTask = (id: number) => {
-    patchState(this.store, () => ({
-      tasks: this.store.tasks.map((task: Task) =>
+    patchState(this.store, (state: TodolistState) => ({
+      tasks: state.tasks.map((task: Task) =>
         task.id === id ? { ...task, completed: !task.completed } : task
       ),
     }));
   };
 
   deleteTask = (id: number) => {
-    patchState(this.store, () => ({
-      tasks: this.store.tasks.filter((task: Task) => task.id !== id),
+    patchState(this.store, (state: TodolistState) => ({
+      tasks: state.tasks.filter((task: Task) => task.id !== id),
     }));
   };
 
