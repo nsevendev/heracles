4d3020a057faf2d9e2d02cb16bcb94df93d07ebd
jha <j.haimez@vattenfall.fr>
2025-02-10 06:44:14 +0100

    chore(#22) delete transition it's bug for routerLink

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,7 +14,6 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
     providePrimeNG({
-      ripple: true,
       theme: {
         preset: heraclesTheme,
         options: {
