f147986a7de2c00e43f74f8599537a682fd2b317
BoissonMathis <math.boiss@laposte.net>
2025-02-10 17:03:59 +0100

    blablabla #24

--- src/shared/components/input-text/input-text.component.spec.ts
+++ src/shared/components/input-text/input-text.component.spec.ts
@@ -1,16 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InputTextComponent } from './input-text.component';
-
+// oui
 describe('InputTextComponent', () => {
   let component: InputTextComponent;
   let fixture: ComponentFixture<InputTextComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [InputTextComponent]
-    })
-    .compileComponents();
+      imports: [InputTextComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(InputTextComponent);
     component = fixture.componentInstance;
