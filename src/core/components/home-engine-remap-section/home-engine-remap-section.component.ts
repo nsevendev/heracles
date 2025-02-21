fe7d3e45a627591a2032c131b4d733fb7ee48657
BoissonMathis <math.boiss@laposte.net>
2025-02-21 13:57:16 +0100

    create home-engine-remap-section #60

--- src/core/components/home-engine-remap-section/home-engine-remap-section.component.ts
+++ src/core/components/home-engine-remap-section/home-engine-remap-section.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'her-home-engine-remap-section',
   imports: [],
   templateUrl: './home-engine-remap-section.component.html',
-  styleUrl: './home-engine-remap-section.component.css'
+  styleUrl: './home-engine-remap-section.component.css',
 })
 export class HomeEngineRemapSectionComponent {
-
+  backgroundImage = 'url("../../../assets/images/engine-remap-home.webp")';
 }
