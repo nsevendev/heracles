b6f253034f1ba2580e741e14e04ecf136dfe127d
BoissonMathis <math.boiss@laposte.net>
2025-02-23 21:52:00 +0100

    add engineRemap.service #60

---
+++ src/domain/interface-infos/service/engine-remap.service.ts
@@ -0,0 +1,6 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InterfaceInfosMethods } from '../store/method';
+
+@Injectable({ providedIn: 'root' })
+export class EngineRemapService {}
