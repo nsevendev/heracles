3aa9d19071291f83ad79a2b1ccf0e7bbf73f542d
BoissonMathis <math.boiss@laposte.net>
2025-03-03 10:57:00 +0100

    inject store in home-page component #60

--- src/core/pages/public/home-page/home-page.component.ts
+++ src/core/pages/public/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { HomeEngineRemapSectionComponent } from '../../../components/home-engine-remap-section/home-engine-remap-section.component';
+import { InterfaceInfosStore } from '../../../../domain/interface-infos/store/interface-infos.root';
 
 @Component({
   selector: 'her-home-page',
@@ -7,4 +8,12 @@ import { HomeEngineRemapSectionComponent } from '../../../components/home-engine
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  private store = inject(InterfaceInfosStore);
+
+  engineRemapInfo = this.store.engineRemap;
+
+  constructor() {
+    this.store.loadEngineRemap();
+  }
+}
